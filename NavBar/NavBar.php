2c1dbe9d583b91616fb055491157f1d7f978d113
Anas <anasmony15@gmail.com>
2025-03-30 17:35:41 +0200

    the footer and the navbar done

---
+++ NavBar/NavBar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../Assets/bootstrap.min.css">
+    <link rel="stylesheet" href="../Assets/style.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+</head>
+<body>
+  <!-- navebar -->
+
+    <nav
+      class="navbar z-2 position-fixed d-flex justify-content-between align-items-center w-100 bg-black"
+    >
+      <div class="container w-100">
+        <p class="title">SP</p>
+        <div class="links mt-3">
+          <ul class="list-unstyled d-flex flex-row">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div class="logo">
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-dribbble"></i>
+        </div>
+      </div>
+    </nav>
+
+
+
+</body>
+</html>
